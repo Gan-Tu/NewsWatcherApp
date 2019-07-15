689d84f431708125649e31bf1a3e4732813a2826
Gan Tu <tugan@berkeley.edu>
2019-07-15 16:30:29 -0700

    feat: use cookies for storing logging in token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withCookies } from 'react-cookie'
 import Bar from './Bar';
 import Footer from './Footer';
 import Navigation from './Navigation';
@@ -16,19 +17,28 @@ class App extends Component {
 
   componentDidMount() {
       fetch("/api/homeNews")
-          .then((res) => { return res.json() })
+          .then(res => {
+            if (!res.ok) {
+              throw res;
+            }
+            return res.json()
+          })
           .then(data => {
             this.setState({
               stories: data,
               totalPages: Math.ceil(data.length / this.state.newsPerPage)
             });
+          })
+          .catch(err => {
+            console.log("Failed to fetch home new");
           });
   }
 
   render() {
     return (
       <div className="App">
-        <Bar title="NewsWatcher | A sample app by Gan" />
+        <Bar title="NewsWatcher | A sample app by Gan"
+             cookies={this.props.cookies}/>
         <Navigation />
         <div className="main-container">
           <NewsListing stories={this.state.stories}
@@ -41,4 +51,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withCookies(App);
--- src/Bar.js
+++ src/Bar.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
-
 import LoginModal from './components/LoginModal';
 import SignupModal from './components/SignupModal';
+
+const DUMMY_TOKEN = "bf9026e7eafd";
+const COOKIE_MAXAGE = 24*60*60;
 
 class Bar extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            loggedIn: false,
-            authToken: null
+        var token = this.props.cookies.get("authToken");
+        if (token && token === DUMMY_TOKEN) {
+            this.state = {
+                loggedIn: true,
+                authToken: token,
+                email: null,
+                password: null,
+                displayName: null
+            };
+        } else {
+            this.state = {
+                loggedIn: false,
+                authToken: this.props.cookies.get("authToken")
+            };
         }
     }
 
     handleLogin = (e) => {
-        this.setState({
-            loggedIn: true
-        });
-        // e.preventDefault();
+        var token = this.props.cookies.get("authToken");
+        // fetch("/api/session", {
+        //     method: "POST",
+        //     body: JSON.stringify({
+        //         email: this.state.email,
+        //         password: this.state.password
+        //     }),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // }).then(res => {
+        //     if (res.ok && res.status === 200) {
+        //         return res.json();
+        //     } else {
+        //         throw Error(res.)
+        //     }
+        // }).catch(err)
+        // displayName: displayName,
+        //                 userId: userId,
+        //                 token: token,
+        if (token && token === DUMMY_TOKEN) {
+            this.setState({
+                loggedIn: true,
+                authToken: token
+            });
+        }
     }
 
     handleLogout = (e) => {
@@ -25,32 +60,12 @@ class Bar extends Component {
             loggedIn: false,
             authToken: null
         });
-        // e.preventDefault();
+        this.props.cookies.remove("authToken");
+        window.location.reload();
     }
 
     render() {
         var menuButtons;
-        if (!this.state.loggedIn) {
-            menuButtons = (
-                <ul className="menu-horizontal">
-                    <li>
-                        <LoginModal handleLogin={this.handleLogin} />
-                    </li>
-                    <li>
-                        <SignupModal sideImg="/img/cowork-11.jpg" />
-                    </li>
-                </ul>
-            );
-        } else {
-            menuButtons = (
-                <ul className="menu-horizontal">
-                    <li>
-                        <a href="#" onClick={this.handleLogout}> Sign Out</a>
-                    </li>
-                </ul>
-            );
-        }
-
         return (
             <section className="bar bar-3 bar--sm bg--secondary">
                 <div className="container">
@@ -66,7 +81,20 @@ class Bar extends Component {
                         {/* Bar action buttons */}
                         <div className="col-lg-6 text-right text-left-xs text-left-sm">
                             <div className="bar__module">
-                                { menuButtons }
+                                <ul className="menu-horizontal">
+                                    <li style={{display: this.state.loggedIn ?
+                                                            "none" : "inline"}}>
+                                        <LoginModal handleLogin={this.handleLogin} />
+                                    </li>
+                                    <li style={{display: this.state.loggedIn ?
+                                                            "inline" : "none"}}>
+                                        <a href="#" onClick={this.handleLogout}> Sign Out</a>
+                                    </li>
+                                    <li style={{display: this.state.loggedIn ?
+                                                            "none" : "inline"}}>
+                                        <SignupModal sideImg="/img/cowork-11.jpg" />
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
